1023320a22a6fad5715a17a120b369d47ca8fc93
unknown <xgen002@naver.com>
2021-11-30 11:52:55 +0900

    second

--- demoloop2.py
+++ demoloop2.py
@@ -4,3 +4,10 @@ lst = [1,2,3]
 
 for i in lst:
     print(i)
+
+print("---break 구문---")
+lst = [1,2,3,4,5,6,7,8,9,10]
+for item in lst:
+    if item > 5:
+        break
+    print("Item:{0}".format(item))
